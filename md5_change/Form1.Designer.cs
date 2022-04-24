4337b88c922c4873fa3a5887038991c99cbd2c91
marlkiller <marlkiller@voidm.com>
2022-04-24 23:48:32 +0800

    'dev'

---
+++ md5_change/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+namespace md5_change
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.file = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.src_md5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.new_md5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.len = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.msg = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lable_msg = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lable_process = new System.Windows.Forms.ToolStripStatusLabel();
+            this.bar_process = new System.Windows.Forms.ToolStripProgressBar();
+            this.lable_total = new System.Windows.Forms.ToolStripStatusLabel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.item_append = new System.Windows.Forms.ToolStripMenuItem();
+            this.item_remove = new System.Windows.Forms.ToolStripMenuItem();
+            this.item_remove_all = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.item_start = new System.Windows.Forms.ToolStripMenuItem();
+            this.item_start_once = new System.Windows.Forms.ToolStripMenuItem();
+            this.item_start_auto = new System.Windows.Forms.ToolStripMenuItem();
+            this.item_stop = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.file,
+            this.src_md5,
+            this.new_md5,
+            this.len,
+            this.msg});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(801, 233);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDown);
+            // 
+            // file
+            // 
+            this.file.Text = "file";
+            this.file.Width = 237;
+            // 
+            // src_md5
+            // 
+            this.src_md5.Text = "src_md5";
+            this.src_md5.Width = 225;
+            // 
+            // new_md5
+            // 
+            this.new_md5.Text = "new_md5";
+            this.new_md5.Width = 215;
+            // 
+            // len
+            // 
+            this.len.Text = "len";
+            // 
+            // msg
+            // 
+            this.msg.Text = "msg";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lable_msg,
+            this.lable_process,
+            this.bar_process,
+            this.lable_total});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 282);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(825, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lable_msg
+            // 
+            this.lable_msg.Name = "lable_msg";
+            this.lable_msg.Size = new System.Drawing.Size(66, 17);
+            this.lable_msg.Text = "lable_msg";
+            this.lable_msg.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lable_process
+            // 
+            this.lable_process.Name = "lable_process";
+            this.lable_process.Size = new System.Drawing.Size(321, 17);
+            this.lable_process.Spring = true;
+            this.lable_process.Text = "lable_process";
+            this.lable_process.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // bar_process
+            // 
+            this.bar_process.Name = "bar_process";
+            this.bar_process.Size = new System.Drawing.Size(100, 16);
+            // 
+            // lable_total
+            // 
+            this.lable_total.Name = "lable_total";
+            this.lable_total.Size = new System.Drawing.Size(321, 17);
+            this.lable_total.Spring = true;
+            this.lable_total.Text = "0";
+            this.lable_total.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.item_append,
+            this.item_remove,
+            this.item_remove_all,
+            this.toolStripSeparator1,
+            this.item_start,
+            this.item_stop});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(149, 120);
+            // 
+            // item_append
+            // 
+            this.item_append.Name = "item_append";
+            this.item_append.Size = new System.Drawing.Size(148, 22);
+            this.item_append.Text = "添加文件";
+            this.item_append.Click += new System.EventHandler(this.item_append_Click);
+            // 
+            // item_remove
+            // 
+            this.item_remove.Name = "item_remove";
+            this.item_remove.Size = new System.Drawing.Size(148, 22);
+            this.item_remove.Text = "刪除当前文件";
+            this.item_remove.Click += new System.EventHandler(this.item_remove_Click);
+            // 
+            // item_remove_all
+            // 
+            this.item_remove_all.Name = "item_remove_all";
+            this.item_remove_all.Size = new System.Drawing.Size(148, 22);
+            this.item_remove_all.Text = "刪除所有文件";
+            this.item_remove_all.Click += new System.EventHandler(this.item_remove_all_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(145, 6);
+            // 
+            // item_start
+            // 
+            this.item_start.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.item_start_once,
+            this.item_start_auto});
+            this.item_start.Name = "item_start";
+            this.item_start.Size = new System.Drawing.Size(148, 22);
+            this.item_start.Text = "开始";
+            // 
+            // item_start_once
+            // 
+            this.item_start_once.Name = "item_start_once";
+            this.item_start_once.Size = new System.Drawing.Size(136, 22);
+            this.item_start_once.Text = "一次";
+            this.item_start_once.Click += new System.EventHandler(this.item_start_once_Click);
+            // 
+            // item_start_auto
+            // 
+            this.item_start_auto.Name = "item_start_auto";
+            this.item_start_auto.Size = new System.Drawing.Size(136, 22);
+            this.item_start_auto.Text = "循环无限次";
+            this.item_start_auto.Click += new System.EventHandler(this.item_start_auto_Click);
+            // 
+            // item_stop
+            // 
+            this.item_stop.Name = "item_stop";
+            this.item_stop.Size = new System.Drawing.Size(148, 22);
+            this.item_stop.Text = "停止";
+            this.item_stop.Click += new System.EventHandler(this.item_stop_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "MSI文件(*.MSI)|*.msi";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 304);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader file;
+        private System.Windows.Forms.ColumnHeader src_md5;
+        private System.Windows.Forms.ColumnHeader new_md5;
+        private System.Windows.Forms.ColumnHeader len;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lable_msg;
+        private System.Windows.Forms.ToolStripStatusLabel lable_process;
+        private System.Windows.Forms.ToolStripProgressBar bar_process;
+        private System.Windows.Forms.ToolStripStatusLabel lable_total;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem item_append;
+        private System.Windows.Forms.ToolStripMenuItem item_remove;
+        private System.Windows.Forms.ColumnHeader msg;
+        private System.Windows.Forms.ToolStripMenuItem item_remove_all;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem item_start;
+        private System.Windows.Forms.ToolStripMenuItem item_start_once;
+        private System.Windows.Forms.ToolStripMenuItem item_start_auto;
+        private System.Windows.Forms.ToolStripMenuItem item_stop;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ md5_change/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace md5_change
+{
+    public partial class Form1 : Form
+    {
+        public bool statu = false;
+        private static object _lock = new object();
+
+        // 循环等待时间:单位秒
+        private static int waitTime = 1;
+
+        Thread thread;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            //AppendFile("C:\\Users\\voidm\\Desktop\\dev\\tmp\\Jar2Exe_src.msi");
+            lable_msg.Text = "请添加文件!";
+
+
+        }
+
+        private void AppendFile(string file)
+        {
+            if (IsExistsItem(file))
+            {
+                return;
+            }
+                
+            ListViewItem item = new ListViewItem(file);
+            ListViewItem.ListViewSubItem itemMd5Src = new ListViewItem.ListViewSubItem();
+            itemMd5Src.Text = GetMD5HashFromFile(file);
+            item.SubItems.Add(itemMd5Src);
+
+            ListViewItem.ListViewSubItem itemMd5New = new ListViewItem.ListViewSubItem();
+            itemMd5New.Text = "";
+            item.SubItems.Add(itemMd5New);
+
+            ListViewItem.ListViewSubItem itemLen = new ListViewItem.ListViewSubItem();
+            itemLen.Text = GetLenFile(file).ToString();
+            item.SubItems.Add(itemLen);
+
+
+            ListViewItem.ListViewSubItem itemCount = new ListViewItem.ListViewSubItem();
+            // TODO 暂未定义
+            itemCount.Text = "TBD_msg";
+            item.SubItems.Add(itemCount);
+
+            listView1.Items.Add(item);
+        }
+
+        public static long GetLenFile(string fileName)
+        {
+            using (FileStream fileStream = new FileStream(fileName, FileMode.Open))
+            {
+                return fileStream.Length;
+            }
+        }
+
+        /// <summary>
+        /// 获取文件MD5值
+        /// </summary>
+        /// <param name="fileName">文件绝对路径</param>
+        /// <returns>MD5值</returns>
+        public static string GetMD5HashFromFile(string fileName)
+        {
+            using (FileStream fileStream = new FileStream(fileName, FileMode.Open))
+            {
+                System.Security.Cryptography.MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+                byte[] retVal = md5.ComputeHash(fileStream);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < retVal.Length; i++)
+                {
+                    sb.Append(retVal[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+
+        private void item_start_once_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count == 0)
+            {
+                lable_msg.Text = "当前文件列表为空,请添加!";
+                return;
+            }
+            SetStatu(true);
+            thread = new Thread(ExecuteChangeMd5);
+            thread.IsBackground = true;
+            thread.Name = "ONCE";
+            thread.Start();
+
+        }
+
+        private void SetStatu(bool s)
+        {
+            lock (_lock)
+            {
+                statu = s;
+            }
+        }
+
+        private bool GetStatu()
+        {
+            lock (_lock)
+            {
+                return statu;
+            }
+        }
+
+        private void ExecuteChangeMd5()
+        {
+            RefreshContextBtm(false);
+
+            while (GetStatu())
+            {
+                Random random = new Random();
+                int newDataLen = 4;
+                byte[] newData = new byte[newDataLen];
+                for (int j = 0; j < newDataLen; j++)
+                {
+                    newData[j] = (byte)random.Next(0, 9);
+                }
+
+                int total_count = listView1.Items.Count;
+                // bar_process.Maximum = total_count;
+                for (int i = 0; i < total_count; i++)
+                {
+                    if (!GetStatu())
+                    {
+                        lable_msg.Text = "已停止!!";
+                        return;
+                    }
+
+                    ListViewItem item = listView1.Items[i];
+                    System.Drawing.Color backColor = item.BackColor;
+                    item.BackColor = System.Drawing.Color.Blue;
+                    int position = int.Parse(item.SubItems[3].Text);
+                    string fileName = item.Text;
+                    lable_msg.Text = $"Execute : {fileName}";
+                    lable_process.Text = $"{i + 1}/{total_count}";
+                    // bar_process.Value = i + 1;
+
+                    FileStream writeStream = File.OpenWrite(fileName);
+                    writeStream.Seek(position, SeekOrigin.Begin);
+                    writeStream.Write(newData, 0, newData.Length);
+                    writeStream.Close();
+
+                    item.SubItems[2].Text = GetMD5HashFromFile(fileName);
+                    item.BackColor = backColor;
+
+                }
+                lable_total.Text = (int.Parse(lable_total.Text) + 1).ToString();
+
+                if (Thread.CurrentThread.Name == "ONCE")
+                {
+                    SetStatu(false);
+                    RefreshContextBtm(true);
+                    lable_msg.Text = "执行完毕";
+                    break;
+                }
+
+                Thread.Sleep(waitTime*1000);
+
+            }
+
+        }
+
+
+        private void stop_task()
+        {
+            if (thread != null)
+            {
+                while (thread.IsAlive)
+                {
+                    contextMenuStrip1.Items[5].Text = "停止中..";
+                    Thread.Sleep(3000);
+                }
+            }
+            
+            RefreshContextBtm(true);
+            contextMenuStrip1.Items[5].Text = "停止";
+            contextMenuStrip1.Items[5].Enabled = true;
+            lable_msg.Text = "执行完毕";
+        }
+        private void item_stop_Click(object sender, EventArgs e)
+        {
+            SetStatu(false);
+            contextMenuStrip1.Items[5].Enabled = false;
+            Thread thread = new Thread(stop_task);
+            thread.IsBackground = true;
+            thread.Name = "STOP";
+            thread.Start();
+        }
+
+        private void RefreshContextBtm(bool flag)
+        {
+            contextMenuStrip1.Items[0].Enabled = flag;
+            contextMenuStrip1.Items[1].Enabled = flag;
+            contextMenuStrip1.Items[2].Enabled = flag;
+            contextMenuStrip1.Items[4].Enabled = flag;
+        }
+
+
+        private void listView1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Right)
+            {
+                return;
+            }
+            contextMenuStrip1.Show(listView1, e.Location);
+        }
+
+        private void item_start_auto_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count == 0)
+            {
+                lable_msg.Text = "当前文件列表为空,请添加!";
+                return;
+            }
+                
+            SetStatu(true);
+            thread = new Thread(ExecuteChangeMd5);
+            thread.IsBackground = true;
+            thread.Name = "FUCKIT";
+            thread.Start();
+        }
+
+        private void item_remove_Click(object sender, EventArgs e)
+        {
+            for (int i = listView1.SelectedItems.Count - 1; i >= 0; i--)
+            {
+                ListViewItem item = listView1.SelectedItems[i];
+                listView1.Items.Remove(item);
+            }
+        }
+
+        private void item_remove_all_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+
+        private void item_append_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            string[] files = openFileDialog1.FileNames;
+            for (int i = 0; i < files.Length; i++)
+            {
+                AppendFile(files[i]);
+            }
+        }
+
+        private bool IsExistsItem(string text)
+        {
+            foreach (ListViewItem item in listView1.Items)
+            {
+                if (item.Text == text)
+                    return true;
+            }
+            return false;
+        }
+
+       
+    }
+}
